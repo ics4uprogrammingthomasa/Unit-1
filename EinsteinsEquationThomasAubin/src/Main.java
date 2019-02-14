b46f43f9a10b0d540a57309257715eb89b356264
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2019-02-14 13:08:56 -0500

    Updated Einstein's Equation

--- EinsteinsEquationThomasAubin/src/Main.java
+++ EinsteinsEquationThomasAubin/src/Main.java
@@ -3,7 +3,7 @@
  * Created on: February 12, 2019
  * Created for: ICS4U
  * Day #4 (Einstein's Equation)
- * This program calculates how many logs can fit onto a truck regarding weight
+ * This program calculates how much energy could be released from user's inputed mass
 */
 
 public class Main {
