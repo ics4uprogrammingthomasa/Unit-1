773d976e401a90624a2da98a2f6ec6d2911ef628
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2019-02-19 13:11:09 -0500

    Updated

--- EinsteinsEquationThomasAubin/src/Main.java
+++ EinsteinsEquationThomasAubin/src/Main.java
@@ -1,3 +1,6 @@
+// Add scanner library
+import java.util.Scanner;
+
 /*
  * Created by: Thomas Aubin
  * Created on: February 12, 2019
@@ -7,7 +10,36 @@
 */
 
 public class Main {
+	// Declare variables
+	static double userInput;
+	static double energyReleased;
+	static double SPEEDOFLIGHT = 299792458;
+	
 	public static void main(String[] args) {
+		// Create console reader object
+		Scanner reader = new Scanner(System.in);
+						
+		// Ask the user mass size
+		System.out.println("What's the mass of your object in kg?");
+				
+		// Save users input
+		try {
+			// Save user input
+			userInput = reader.nextDouble();
+		}
+		// If an error occurs, tell the user to input a proper mass (Number)
+		catch(Exception e) {
+			// Tell the user to input a proper mass
+			System.out.println("Please input a proper mass");
+		}
 		
+		// Close reader
+		reader.close();
+		
+		// Calculate energyReleased with set mass
+		energyReleased = userInput * Math.pow(SPEEDOFLIGHT, 2);
+		
+		// Tell user energyReleased
+		System.out.println("Max energy released is " + energyReleased + " J");
 	}
 }
